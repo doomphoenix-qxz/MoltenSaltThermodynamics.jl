a1c3343277dd9707b4e7bb25e80e668ea9ddd884
doomphoenix-qxz <richardlfitzhugh@gmail.com>
2020-09-29 16:49:29 -0600

    Modify MoltenSaltThermodynamics.jl to include recent developments

--- src/MoltenSaltThermodynamics.jl
+++ src/MoltenSaltThermodynamics.jl
@@ -4,6 +4,7 @@ using Statistics: mean
 using Plots
 using LinearAlgebra
 using LaTeXStrings
+using Optim
 
 const R_GAS = 1.380649*6.02214076 # Boltzmann*Avogadro, values from CODATA18
 const FARADAY = 96485.3365
@@ -11,7 +12,8 @@ const FARADAY = 96485.3365
 include("extrapolation.jl")
 include("modifiedquasichemical.jl")
 include("numericalmethods.jl")
+include("test_mqcm.jl")
 
-export infinitedilution_extrapolate, plot_model, compare_actcoeffs 
+export infinitedilution_extrapolate, plot_model, compare_actcoeffs, mytest1
 
 end # module
